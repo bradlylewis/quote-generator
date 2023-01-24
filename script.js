656e174d2a2e4ab01e59d0136715cd760f995444
Bradly Lewis <bradlylewisdev@gmail.com>
2023-01-24 18:59:59 -0500

    push to github

---
+++ script.js
@@ -0,0 +1,56 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+let apiQuotes = [];
+
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function removeLoadingSpinner() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
+// Show New Quote
+function newQuote() {
+    showLoadingSpinner();
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length + 1)];
+    quote.text.length > 120 ? quoteText.classList.add('long-quote') : quoteText.classList.remove('long-quote');
+    quoteText.textContent = quote.text;
+    authorText.textContent = quote.author || 'Unknown';
+    removeLoadingSpinner();
+}
+
+// Get Quotes From API
+async function getQuotes() {
+    showLoadingSpinner();
+    const apiURL = "https://jacintodesign.github.io/quotes-api/data/quotes.json"
+    try {
+        const response = await fetch(apiURL);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https:///twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
+// getQuotes();
+getQuotes();
